3f7a306175a81052b3a7b2da976409affafef37f
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-17 02:55:04 -0500

    Update home.component.ts

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -9,12 +9,21 @@ import { Show } from "../../Interfaces/Show";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  showArray:Show;
+  showArray:Show[];
+  showList:Show[];
 
   constructor(private ss:ShowService) { }
 
   ngOnInit(): void {
-    // this.showList = this.ss.sendList();
+    this.fillList();
+  }
+
+  fillList(){
+    this.ss.sendList(this.showArray).then(list=>this.showList=list)
+  }
+
+  addShow (title:Show){
+    this.ss.newShow(title).then((item)=>{this.showList.push(item)});
   }
 
 }
