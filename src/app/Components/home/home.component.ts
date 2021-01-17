d5b537293c1eead6c4d9f32c712ad45b578cb49e
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-16 21:16:55 -0500

    Update home.component.ts

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { fromEventPattern } from 'rxjs';
+
 import { ShowService } from 'src/app/Services/show.service';
 import { Show } from "../../Interfaces/Show";
 
@@ -9,17 +9,12 @@ import { Show } from "../../Interfaces/Show";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  // showArray:Show[];
-  show:any;
-  showList:any;
+  showArray:Show;
 
   constructor(private ss:ShowService) { }
 
   ngOnInit(): void {
-    this.showList = this.ss.sendList();
-    // for(let i=0; i<this.showList.length;i++){
-    //   this.showArray.push(this.showList[i]);
-    // }
+    // this.showList = this.ss.sendList();
   }
 
 }
