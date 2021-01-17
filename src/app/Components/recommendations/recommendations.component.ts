deb36dee7cb30b75b8fefab14f6cf18112bc41b5
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-17 02:55:57 -0500

    Update recommendations.component.ts

--- src/app/Components/recommendations/recommendations.component.ts
+++ src/app/Components/recommendations/recommendations.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
-
-import show_list from "../../../assets/recommendations.json";
 import { Show } from "../../Interfaces/Show";
 
 @Component({
@@ -10,16 +8,18 @@ import { Show } from "../../Interfaces/Show";
   styleUrls: ['./recommendations.component.css']
 })
 export class RecommendationsComponent implements OnInit {
-  @Output() sendList: EventEmitter<any> = new EventEmitter();
-  item:Show;
-  fullList:Show = show_list;
+  @Input() item:Show;
 
   constructor() { }
 
   ngOnInit(): any {
-    // this.sendList.emit(this.fullList);
   }
 
-
+  setClasses() {
+    let classes = {
+      'title-text': true
+    }
+    return classes;
+  }
 
 }
