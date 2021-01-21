35dc9519fced4edd10c682d091abc18770fceb11
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-21 00:59:24 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "../app/Components/home/home.component";
+import { TopShowsComponent } from './Components/top-shows/top-shows.component';
+import { CurrentlyAiringComponent } from "./Components/currently-airing/currently-airing.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'top_shows', component: TopShowsComponent},
+  {path: 'currently_airing', component: CurrentlyAiringComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
