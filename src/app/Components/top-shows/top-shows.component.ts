e60b93f9545d2de043e8f752d606ea5739b2cf87
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-22 01:01:11 -0500

    Update top-shows.component.ts

--- src/app/Components/top-shows/top-shows.component.ts
+++ src/app/Components/top-shows/top-shows.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ShowService } from "../../Services/show.service";
+
+import { Top_Shows } from "../../Interfaces/Top_Shows";
 
 @Component({
   selector: 'app-top-shows',
@@ -6,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-shows.component.css']
 })
 export class TopShowsComponent implements OnInit {
-
-  constructor() { }
+  topShows:Top_Shows[];
+  sel_Show:Top_Shows;
+  constructor(private ss:ShowService) { }
 
   ngOnInit(): void {
+    this.ss.sendTopShows().subscribe(data=>this.topShows=data);
   }
-
 }
