83ece6b64d9b40012aaffde16fc414aa85fffbaf
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-23 22:09:07 -0500

    Update show.service.ts

--- src/app/Services/show.service.ts
+++ src/app/Services/show.service.ts
@@ -3,20 +3,27 @@ import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Show } from "../Interfaces/Show";
+import { Top_Shows } from "../Interfaces/Top_Shows";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowService {
-  jsonURL:string = '/assets/recommendations.json'
+  recJsonURL:string = '/assets/recommendations.json';
+  topJsonURL:string = '/assets/top_2018_shows.json';
 
   constructor(private http:HttpClient) { }
 
   sendList():Observable<Show[]>{
-    return this.http.get<Show[]>(this.jsonURL);
+    return this.http.get<Show[]>(this.recJsonURL);
   }
 
   newShow(item:Show):Promise<Show>{
     return new Promise(resolve=>resolve(item))
   }
+
+  sendTopShows():Observable<Top_Shows[]>{
+    return this.http.get<Top_Shows[]>(this.topJsonURL);
+  }
+
 }
